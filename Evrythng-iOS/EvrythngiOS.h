2ce3774ef01f470a06926f464ed62155609bb19b
JD Castro <jd@imfreemobile.com>
2017-06-05 22:05:49 +0800

    0.0.93

--- Evrythng-iOS/EvrythngiOS.h
+++ Evrythng-iOS/EvrythngiOS.h
@@ -16,5 +16,3 @@ FOUNDATION_EXPORT const unsigned char Evrythng_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Evrythng_iOS/PublicHeader.h>
 
-
-#import "GoogleMobileVision"
